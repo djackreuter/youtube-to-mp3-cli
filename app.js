58d43bd4aab1461b5a58f46d0d3b12b5198c38f7
Dylan Reuter <djreuter45@gmail.com>
2018-11-08 21:29:35 -0600

    started adding options

---
+++ app.js
@@ -0,0 +1,21 @@
+const yargs = require('yargs');
+const { convertVideo } = require('./ytdl');
+
+const argv = yargs.options({
+  url: {
+    alias: 'u',
+    describe: 'URL of the YouTube video to convert',
+    type: 'string'
+  },
+  name: {
+    alias: 'n',
+    describe: 'Name to same the file as',
+    type: 'string'
+  }
+})
+.demandOption(['url', 'name'], 'Please provide a url and name for the file')
+.help()
+.alias('help', 'h')
+.argv
+
+convertVideo(argv.url, argv.name);
